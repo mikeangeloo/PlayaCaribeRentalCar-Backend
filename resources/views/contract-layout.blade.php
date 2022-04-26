6b29079fe2b8450ad304c59dac62517dc17d8229
mikeangeloo <mikeangeloo87@outlook.com>
2022-04-26 08:24:16 -0500

    APOLLO-1 webpack laravel config

---
+++ resources/views/contract-layout.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Contract</title>
+    <link rel="stylesheet" href="{{'css/main.css'}}">
+</head>
+<body>
+    <div class="contract-container">
+        <div class="row no-gutters">
+            <div class="col-6">
+                <img class="img-fluid" src="{{ 'assets/img/PDF-Logo.png' }}">
+            </div>
+            <div class="col-3">
+                <div>
+                    <p class="text-uppercase">ANEXO</p>
+                    <p>--</p>
+                </div>
+            </div>
+            <div class="col-3">
+                <p class="text-uppercase">CONTRATO#: F3939393</p>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,8 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+mix.sass('resources/sass/main.scss', 'public/css');
+
+mix.browserSync({
+    proxy: '192.168.1.101:8020'
+});
