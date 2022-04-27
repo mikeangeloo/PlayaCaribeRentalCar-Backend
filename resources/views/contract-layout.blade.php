26ed454be5014d1ce020732ee0c22e7025e84ee3
mikeangeloo <mikeangeloo87@outlook.com>
2022-04-26 23:25:02 -0500

    APOLLO-1 Agregando estilos y maqueta primera hoja

--- resources/views/contract-layout.blade.php
+++ resources/views/contract-layout.blade.php
@@ -9,18 +9,297 @@
 </head>
 <body>
     <div class="contract-container">
-        <div class="row no-gutters">
+        <!-- CONTRACT HEADER -->
+        <div class="row no-gutters align-content-center">
             <div class="col-6">
                 <img class="img-fluid" src="{{ 'assets/img/PDF-Logo.png' }}">
             </div>
-            <div class="col-3">
-                <div>
-                    <p class="text-uppercase">ANEXO</p>
-                    <p>--</p>
+            <div class="col-3 d-flex flex-column align-self-center">
+                <p class="m-0 text-uppercase">ANEXO</p>
+                <p class="m-0">--</p>
+            </div>
+            <div class="col-3 d-flex align-items-center">
+                <p class="text-uppercase">CONTRATO#: F3939393</p>
+            </div>
+        </div> <!-- END CONTRACT HEADER -->
+
+        <!-- SUBHEADE DETAILS -->
+        <div class="row no-gutters subhead-contract">
+             <!-- CLIENT DATA -->
+            <div class="column client-data">
+                <p>C Tigrillo MZA 12 L1 COZUMEL QROO 777646</p>
+                <p>9872727272</p>
+                <p><b>test@mail.com.mx</b></p>
+            </div> <!-- END CLIENT DATA -->
+
+            <!-- END DEPARTURE OFFICE -->
+            <div class="column">
+                <div class="row no-gutters">
+                    <div class="col-6 p-1"><b>OFICINA DE RENTA</b></div>
+                    <div class="col-6 p-1" style="text-align: right"><b>FPDC60</b></div>
                 </div>
+                <div class="row no-gutters">
+                    <div class="col-12 p-1">
+                        <p>PDC PLAZA TUKAN AV 10 ESQUINA 16 PLAYA DEL CARMEN 77710</p>
+                    </div>
+                </div>
+            </div> <!-- END DEPARTURE OFFICE -->
+
+            <!-- RENT DATE -->
+            <div class="column date-time flex-15">
+                <p><b>FECHA DE RENTA</b></p>
+                <p>23-NOV-2022</p>
+                &nbsp;
+                <p><b>HORA</b></p>
+                <p>02:57 PM</p>
+            </div> <!-- END RENT DATE -->
+
+            <!-- RETURN OFFFICE -->
+            <div class="column">
+                <div class="row no-gutters">
+                    <div class="col-6 p-1"><b>OFICINA DE RETORNO</b></div>
+                    <div class="col-6 p-1" style="text-align: right"><b>FPDC60</b></div>
+                </div>
+                <div class="row no-gutters">
+                    <div class="col-12 p-1">
+                        <p>PDC PLAZA TUKAN AV 10 ESQUINA 16 PLAYA DEL CARMEN 77710</p>
+                    </div>
+                </div>
+            </div>  <!-- END RETURN OFFFICE -->
+
+            <!-- RETURN DATE -->
+            <div class="column  date-time flex-15">
+                <p><b>FECHA DE RETORNO</b></p>
+                <p>26-NOV-2022</p>
+                &nbsp;
+                <p><b>HORA</b></p>
+                <p>01:00 PM</p>
+            </div> <!-- END RETURN DATE -->
+
+        </div> <!-- END SUBHEADE DETAILS -->
+
+        <!-- FIRST ROW DETAILS -->
+        <div class="row">
+            <div class="col-3">
+                <p><b>TARIFAS PACTADAS</b></p>
+                <p><b>CDP:</b>&nbsp;PREPAGO02F 50% DCTP PREPAGO</p>
             </div>
             <div class="col-3">
-                <p class="text-uppercase">CONTRATO#: F3939393</p>
+                <p><b>CONFIRMACION #:</b>&nbsp;619BCFC1462</p>
+                <p><b>RES LOCAL #:</b>&nbsp;06JJW1</p>
+            </div>
+            <div class="col-3">
+                <p><b>CANAL DE ORIGEN:</b>&nbsp;WEB</p>
+                <p><b>CÓDIGO DE TARIFA:</b>&nbsp;FCATD</p>
+            </div>
+            <div class="col-3">
+                <p><b>NO. DE CLIENTE:</b>&nbsp;Z0KFGG</p>
+                <p><b>CONTACTO:</b>&nbsp;CZM LOCAL</p>
+            </div>
+        </div> <!-- END FIRST ROW DETAILS -->
+
+        <br>
+
+        <div class="row">
+            <div class="col-6">
+                <div class="row">
+                    <div class="col-6">
+                        <p>Clase Reservada</p>
+                        <p>Clase asignada</p>
+                        <p>Clase cargada</p>
+                        <p>No. de Vehiculo</p>
+                        <p>Marca</p>
+                        <p>Modelo</p>
+                        <p>Color</p>
+                        <p>No. de placas</p>
+                        <p>Serial No.</p>
+                        <p>Capacidad del tanque</p>
+                        <p>Gasolina de salida</p>
+                        <p>Kilometraje de salida</p>
+                    </div>
+                    <div class="col-6">
+                        <p>C</p>
+                        <p>D</p>
+                        <p>C</p>
+                        <p>VV1308</p>
+                        <p>VW</p>
+                        <p>VENTO</p>
+                        <p>Blanco</p>
+                        <p>YYP-744-D</p>
+                        <p>MX93938383MT838383</p>
+                        <p>55.0 LITROS</p>
+                        <p>8</p>
+                        <p>15018</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-6">
+                <div class="row mb-2">
+                    <div class="col-12">
+                        <p><b>Tarifa publica vigente MXN</b></p>
+                    </div>
+                </div>
+                <div class="row mb-2">
+                    <div class="col-12 border-arround d-flex align-items-center justify-content-between">
+                        <div>
+                            <p><b>Descripcion de los cargos</b></p>
+                        </div>
+                        <div>
+                            <p>---</p>
+                        </div>
+                        <div>
+                            <p><b>Cargo estimado</b></p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row mb-2">
+                    <table class="items-table">
+                        <thead>
+                            <tr>
+                                <th></th>
+                                <th>Precio.UNIT</th>
+                                <th>DIA</th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td class="pb-1 pt-1 text-decoration-underline text-bold">Tarifas tiempo y kilometraje</td>
+                                <td class="pb-1 pt-1 text-bold"></td>
+                                <td class="pb-1 pt-1 text-bold"></td>
+                                <td class="pb-1 pt-1 text-bold"></td>
+                            </tr>
+                            <tr>
+                                <td class="text-bold">HOURS / HORAS</td>
+                                <td class="text-bold">$187.66</td>
+                                <td class="text-bold"></td>
+                                <td class="text-bold"></td>
+                            </tr>
+                            <tr>
+                                <td class="text-bold">DAYS / DIAS</td>
+                                <td class="text-bold">$750.62</td>
+                                <td class="text-bold">X 3</td>
+                                <td class="text-bold">$2251.66</td>
+                            </tr>
+                            <tr>
+                                <td class="text-bold">WEEKS / SEMANAL</td>
+                                <td class="text-bold">$4503.72</td>
+                                <td class="text-bold"></td>
+                                <td class="text-bold"></td>
+                            </tr>
+                            <tr>
+                                <td class="text-bold">MONTHS / MENSUAL</td>
+                                <td class="text-bold">$21017.38</td>
+                                <td class="text-bold"></td>
+                                <td class="text-bold"></td>
+                            </tr>
+                            <tr>
+                                <td class="text-bold">EX DAYS / DIA EXTRA</td>
+                                <td class="text-bold">$750.62</td>
+                                <td class="text-bold"></td>
+                                <td class="text-bold"></td>
+                            </tr>
+                            <tr>
+                                <td class="text-bold">DESCUENTO T&K / DISCOUNT T&M</td>
+                                <td class="text-bold">50%</td>
+                                <td class="text-bold">X 1</td>
+                                <td class="text-bold">-$1125.93</td>
+                            </tr>
+                            <tr>
+                                <td class="text-bold text-decoration-underline">Cargos de gas</td>
+                                <td class="text-bold"></td>
+                                <td class="text-bold"></td>
+                                <td class="text-bold"></td>
+                            </tr>
+                        </tbody>
+                    </table>
+
+                    <table class="items-table">
+                        <thead>
+                            <tr>
+                                <th></th>
+                                <th>Precio.UNIT</th>
+                                <th>DIA</th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td class="pb-1 pt-1 text-decoration-underline text-bold">PRODUCTOS ADICIONALES</td>
+                                <td class="pb-1 pt-1 text-bold"></td>
+                                <td class="pb-1 pt-1 text-bold"></td>
+                                <td class="pb-1 pt-1 text-bold"></td>
+                            </tr>
+                            <tr>
+                                <td class="text-bold">PREMIUM RETURN</td>
+                                <td class="text-bold">$350.00</td>
+                                <td class="text-bold">X 1</td>
+                                <td class="text-bold">$350.00</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <div class="row mb-2">
+                    <div class="col-12 border-arround d-flex align-items-center justify-content-between">
+                        <div>
+                            <p><b>Subtotal-2</b></p>
+                        </div>
+                        <div>
+                        </div>
+                        <div>
+                            <p><b>$4112.93</b></p>
+                        </div>
+                    </div>
+                </div>
+                <div class="row mb-2">
+                    <table class="items-table">
+                        <thead>
+                            <tr>
+                                <th></th>
+                                <th>Precio.UNIT</th>
+                                <th>DIA</th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td class="pb-1 pt-1 text-decoration-underline text-bold">CUOTAS LOCALES E IMPUESTOS FEDERALES</td>
+                                <td class="pb-1 pt-1 text-bold"></td>
+                                <td class="pb-1 pt-1 text-bold"></td>
+                                <td class="pb-1 pt-1 text-bold"></td>
+                            </tr>
+                            <tr>
+                                <td class="text-bold">CUOTA LOCACION / LOCATION FEE @ 19%</td>
+                                <td class="text-bold"></td>
+                                <td class="text-bold"></td>
+                                <td class="text-bold">$753.53</td>
+                            </tr>
+                            <tr>
+                                <td colspan="4"></td>
+                            </tr>
+                            <tr>
+                                <td class="text-bold">I.V.A / TAX @ 16%</td>
+                                <td class="text-bold"></td>
+                                <td class="text-bold"></td>
+                                <td class="text-bold">$818.15</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <div class="row mb-2">
+                    <div class="col-12 border-arround d-flex align-items-center justify-content-between">
+                        <div>
+                            <p><b>CARGOS ESTIMADOS TOTALES INICIALES X_______</p>
+                            <p style="font-weight: 300; font-size: 9px;">*TODAS LAS CANTIDADES REFLEJADAS SON EN MONEDA NACIONAL (MXN)</p>
+                        </div>
+                        <div>
+                        </div>
+                        <div>
+                            <p><b>$6123.46</b></p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
